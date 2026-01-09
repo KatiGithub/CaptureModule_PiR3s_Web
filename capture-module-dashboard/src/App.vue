049c0097bd1ff254e7e324bca52cbb6059dbd67f
Kati Pairojtanachai <kati.pairojtanachai@gmail.com>
2026-01-09 14:04:17 +0100

    Changed project to quasar framework

--- capture-module-dashboard/src/App.vue
+++ capture-module-dashboard/src/App.vue
@@ -1,11 +1,11 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <router-view />
 </template>
 
-<style scoped></style>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
